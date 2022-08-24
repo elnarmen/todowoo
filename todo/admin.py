54803b37729a4c0a42d6bfeedfcfd444ce13fa3e
Эльнар Менгельбаев <elnar_men@mail.ru>
2022-08-24 12:05:22 +0300

    Admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -4,5 +4,7 @@ from .models import Todo
 
 class TodoAdmin(admin.ModelAdmin):
     readonly_fields = ('created',)
+    list_display = ('id', 'title', 'created', 'datecompleted', 'user')
+    list_display_links = ('id', 'title')
 
 admin.site.register(Todo, TodoAdmin)
--- todo/models.py
+++ todo/models.py
@@ -3,12 +3,17 @@ from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=100)
-    memo = models.TextField(blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    datecompleted = models.DateTimeField(null=True, blank=True)
-    important = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    memo = models.TextField(blank=True, verbose_name='Описание')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    datecompleted = models.DateTimeField(null=True, blank=True, verbose_name='Дата выполнения')
+    important = models.BooleanField(default=False, verbose_name='Важность')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Дело'
+        verbose_name_plural = 'Задачи'
+        ordering = ('-created',)
